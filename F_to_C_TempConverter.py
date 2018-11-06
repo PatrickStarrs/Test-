766a948d24a2583bc9b55f55aa98c71d40c8d9d3
PatrickStarrs <44789629+PatrickStarrs@users.noreply.github.com>
2018-11-05 19:40:13 -0500

    Add files via upload

---
+++ F_to_C_TempConverter.py
@@ -0,0 +1,30 @@
+# This asks for the temperature value from the user
+temperature = input("What is the temperature would you like to change? ")
+type(temperature)
+
+# This converts the user input string into an integer to do MATH
+if str.isdigit(temperature):
+    integer_version_temperature = int(temperature)
+
+# This asks for the temperature unit
+    temp_unit = input("F or C? ")
+    type(temp_unit)
+
+# If it is Farenheit
+    if temp_unit == "F":
+# Do MATH
+       new_temperature = (integer_version_temperature-32 )*(5/9)
+       print("The temperature is ")
+       print(new_temperature)
+
+# If it is Celsius
+    elif temp_unit == "C":
+# Do MATH
+       new_temperature = (integer_version_temperature*(9/5))+32 
+       print("The temperature is ")
+       print(new_temperature)
+
+    else:
+        print("Pick between F or C, dingus!")
+else:
+    print("Pick a number, doofus!")
